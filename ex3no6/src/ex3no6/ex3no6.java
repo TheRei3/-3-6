5cd8f612065baa4c0cf4c84a1013798ed9f2ede6
admin <admin@DESKTOP-8L2IG92>
2021-04-13 06:46:34 +0900

    ex3no6

---
+++ ex3no6/src/ex3no6/ex3no6.java
@@ -0,0 +1,11 @@
+package ex3no6;
+
+public class ex3no6 {  //98p
+    public static void main(String[] args) {
+		byte a = 10;
+		byte b = 20;
+//		byte c =  a + b ;
+		byte c = (byte)( a + b ) ;
+		System.out.println(c);
+	}
+}
